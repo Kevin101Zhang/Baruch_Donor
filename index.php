761514193fe0de7ff6403f3ce7ac7a24d7874101
Kevin101Zhang <42101107+Kevin101Zhang@users.noreply.github.com>
2018-11-06 15:09:25 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>BCTC project</title>
+  <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+  <link rel="stylesheet" href="css/index.css" type="text/css" media="screen" title="no title" charset="utf-8">
+  </head>
+  <body>
+
+<form method="post" action="query.php">
+<h1>Please Log In</h1>
+<p>Username: <input type="text" name="username"></p>
+<p>Password: <input type="password" name="password"></p>
+<p><input type="submit" name="submit" value="Log In"></p>
+</form>
+<!-- fdssdf -->
+  </body>
+</html>
+
+
