02d35f14d307bcb6119aebd38ab2e0fee346e25e
Kevin Zhang <kevin101zhang@gmail.com>
2019-01-02 13:44:53 -0500

    add delete button for pc_id

--- result_php_files/holder.php
+++ result_php_files/holder.php
@@ -1,36 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-    <title>Search For Donors</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
-    <!-- Linking CSS -->
-    <link rel="stylesheet" href="../Assets/CSS/index.css" type="text/css">
-</head>
-
-<body>
-    <?php
-        require_once('../assets/php/connection.php'); //establishes connection to the database
-
-        if(isset($_POST['delete_button'])) {
-            $delete_pc = $_POST['delete_button'];
-            $delete_query = "DELETE FROM computer WHERE pc_id = '$delete_pc' ";
-            mysqli_query($mysql, $delete_query);
-            echo $_POST['delete_button'];
-        }
-
-        //on click of search_donor button
-        if(isset($_POST['submit_search_donor'])){
-            // require('holder.php');
-            // define the list of fields
+// define the list of fields
             $fields = array('donor_id', 'prefix', 'first_name', 'last_name', 'suffix', 'entry_date');
             $conditions = array();
         
@@ -117,13 +87,4 @@
                             <?php
                                     }
                                 }
-        }
                             ?>
-                            <br>
-                    </div>
-                    <button type="submit" class="btn btn-primary" onclick="window.location.href='../index_php_files/user.php'">Return</button>
-                </div>  
-            </div>
-</body>
-
-</html>
